34f1a0affecb6bd27383ab00238214ba2f39ad81
Che Lorde <46496846+Lorde-Che-215510746@users.noreply.github.com>
2023-03-04 18:24:12 -0500

    Added join abaility
    when a member joins #introduction channel the bot will give a generic greeting. Still have to add varying messages, and provide members' name upon joining

--- engagement.py
+++ engagement.py
@@ -1,5 +1,44 @@
+import slack
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from flask import Flask
+from slackeventsapi import SlackEventAdapter
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+app = Flask(__name__)
+slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'],'/slack/events',app)
+
+client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
+
 # make a function that will send a welcome message when a user joins
-print("hiiiiiii")
+
+BOT_ID = client.api_call("auth.test")['user_id']
+
+@slack_event_adapter.on('member_joined_channel') #function has bot collcet information on message from user
+def message(payload):
+    print(payload)
+    event = payload.get('event', {})
+    channel_id = event.get('channel')
+    user_id = event.get('user') #Tito will establish user's NAME rather than ID
+    text = event.get('text')
+    
+
+    if channel_id == "C04T3JPMCGG":
+        client.chat_postMessage(channel=channel_id, text='Hello ' + user_id +' hope you enjoy your stay')
+    
+    # if BOT_ID != user_id:
+    #     client.conversations_join(channel='C04T3JPMCGG',text='Welcome!')
+    
+        # client.chat_postMessage(channel=channel_id, text='Hello ' + user_id +' hope you enjoy your stay')
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
 
 
 # make a function that will give them tasks to do 
